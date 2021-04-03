d93d8564be631c25e8db9c5715aa14a6ea8849cd
John Daniels <john.daniels@masabi.com>
2021-04-03 17:59:16 +0100

    Stop collecting data when out of range

--- src/sensorcalibration/gather-sensor-data.py
+++ src/sensorcalibration/gather-sensor-data.py
@@ -2,22 +2,38 @@ from pybricks.pupdevices import Motor, ColorDistanceSensor
 from pybricks.parameters import Port, Stop
 from pybricks.tools import wait
 
+
+class Buffer:
+    def __init__(self, size):
+        self.size = size
+        self.store = []
+
+    def push(self, item):
+        self.store.append(item)
+        if len(self.store) > self.size:
+            self.store.pop(0)
+
+
 # The number of degrees the motor must rotate to move the test rig by 1m
 DEGREES_PER_METRE = 4822
 
 TEST_DISTANCE_MM = 300
 TEST_INCREMENT_MM = 5
-DEGREES_PER_INCREMENT = DEGREES_PER_METRE * TEST_INCREMENT_MM / 1000
+DEGREES_PER_INCREMENT = round(DEGREES_PER_METRE * TEST_INCREMENT_MM / 1000)
 
 motor = Motor(Port.D)
 sensor = ColorDistanceSensor(Port.B)
 
 results = {}
 distanceFromTarget = 0
-while distanceFromTarget < TEST_DISTANCE_MM:
+recent = Buffer(5)
+
+while distanceFromTarget < TEST_DISTANCE_MM and sum(recent.store) < 500:
     wait(500)
-    results[distanceFromTarget] = sensor.distance()
-    motor.run_angle(150, -DEGREES_PER_INCREMENT, Stop.HOLD)
+    newReading = sensor.distance()
+    results[distanceFromTarget] = newReading
+    recent.push(newReading)
+    motor.run_angle(100, -DEGREES_PER_INCREMENT, Stop.HOLD)
     distanceFromTarget += TEST_INCREMENT_MM
 motor.run_angle(150, 0, Stop.COAST)
 for key in sorted(results):
