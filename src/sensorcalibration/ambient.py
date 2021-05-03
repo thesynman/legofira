185450da93a440a23d57e6a4cad9060c866000e5
John Daniels <john.daniels@masabi.com>
2021-05-03 17:09:40 +0100

    Add ambient sensor check

---
+++ src/sensorcalibration/ambient.py
@@ -0,0 +1,17 @@
+from pybricks.parameters import Port
+from pybricks.tools import wait
+from pybricks.pupdevices import ColorDistanceSensor
+
+cdsensor = ColorDistanceSensor(Port.A)
+
+lastReading = {}
+
+print('Starting...')
+cdsensor.light.off()
+wait(5000)
+while True:
+    wait(100)
+    newReading = cdsensor.ambient()
+    if newReading != lastReading.get("AMB"):
+        print('AMB is %s' % newReading)
+        lastReading["AMB"] = newReading
