a713270c305a80e4d0e646d9576893e0e863f4dc
John Daniels <john.daniels@masabi.com>
2021-05-03 17:10:04 +0100

    Update to suit latest vehicle prototype

--- src/fira-vehicle/vehicle-calib.py
+++ src/fira-vehicle/vehicle-calib.py
@@ -3,11 +3,10 @@ from pybricks.parameters import Port, Stop, Direction
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
-sensor = ColorDistanceSensor(Port.B)
-leftMotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-rightMotor = Motor(Port.C)
-driveBase = DriveBase(leftMotor, rightMotor, 49.7, 156)
+leftMotor = Motor(Port.C, Direction.COUNTERCLOCKWISE, gears=[12,20])
+rightMotor = Motor(Port.D, gears=[12,20])
+driveBase = DriveBase(leftMotor, rightMotor, 49.7, 98.1)
 
 driveBase.settings(120, 200, 100, 150)
-# driveBase.turn(-360)
-driveBase.straight(1000)
+driveBase.turn(-360)
+# driveBase.straight(1000)
--- src/fira-vehicle/vehicle.py
+++ src/fira-vehicle/vehicle.py
@@ -1,18 +1,28 @@
+import pybricks
 from pybricks.pupdevices import Motor, ColorDistanceSensor
-from pybricks.parameters import Port, Stop, Direction
-from pybricks.tools import wait
+from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
+from pybricks.hubs import TechnicHub
 
-sensor = ColorDistanceSensor(Port.B)
-leftMotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-rightMotor = Motor(Port.C)
-driveBase = DriveBase(leftMotor, rightMotor, 49.7, 156)
+print(pybricks.version)
 
-driveBase.settings(120, 200, 100, 150)
+hub = TechnicHub()
+sensor = ColorDistanceSensor(Port.A)
+leftMotor = Motor(Port.C, Direction.COUNTERCLOCKWISE, gears=[12,20])
+rightMotor = Motor(Port.D, gears=[12,20])
+driveBase = DriveBase(leftMotor, rightMotor, 49.7, 98.1)
+
+driveBase.settings(100, 150, 70, 100)
 turn = 90
 peek = 30
 
+battery = None
+
 while True:
+    newBattery = hub.battery.voltage()
+    if battery != newBattery:
+        battery = newBattery
+        print("Battery is %smV" % battery)
     driveBase.straight(100)
     if sensor.distance() == 100:
         driveBase.turn(peek)
